268186ca9ad10dd6c51798d5dc637a70c4eeebfa
juanIgnacioEchaide <jie_chaide@hotmail.com>
2025-06-01 02:18:36 -0300

    user detail children styled

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { UsersEffects } from './features/list/store/effects/users.effect';
 import { usersFeature } from './features/list/store/reducer/users.reducer';
-
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideStore( ),
     provideState(usersFeature),
     provideEffects([UsersEffects]),
+     provideStoreDevtools({ maxAge: 25, logOnly: false }), 
   ],
 };
